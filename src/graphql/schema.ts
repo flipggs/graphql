cb0e01f1ae50ba160eab778e4e59b2b80d70bc05
flipggs <flipggs@gmail.com>
2020-05-08 19:19:11 -0300

    Add schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,46 +1,18 @@
 import { makeExecutableSchema } from "graphql-tools";
+import { Query } from "./query";
+import { Mutation } from "./mutation";
 
-const users: any[] = [
-  {
-    id: 1,
-    name: "John",
-    email: "john@teste.com",
-  },
-  {
-    id: 2,
-    name: "Fulano",
-    email: "fulano@teste.com",
-  },
-];
+import { userTypes } from './resources/user/user.schema';
+import { postTypes } from "./resources/post/post.schema";
+import { commentTypes } from "./resources/comment/comment.schema";
 
-const typeDefs = `
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-  }
-
-  type Query {
-    allUsers: [User!]!
-  }
-
-  type Mutation {
-    createUser(name: String!, email: String!): User
+const SchemaDefinition = `
+  type Schema {
+    query: Query
+    mutation: Mutation
   }
 `;
 
-const resolvers = {
-  Query: {
-    allUsers: () => users,
-  },
-  Mutation: {
-    createUser: (parent, args) => {
-      const newUser = Object.assign({id: users.length + 1}, args);
-      users.push(newUser);
-
-      return newUser;
-    }
-  }
-};
-
-export default makeExecutableSchema({ typeDefs, resolvers });
+export default makeExecutableSchema({
+  typeDefs: [SchemaDefinition, Query, Mutation, userTypes, postTypes, commentTypes],
+});
