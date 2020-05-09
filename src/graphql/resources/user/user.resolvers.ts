6043359f7d1760ae159b986e6d56b0ef4244106f
flipggs <flipggs@gmail.com>
2020-05-09 16:03:11 -0300

    Add resolvers

--- src/graphql/resources/user/user.resolvers.ts
+++ src/graphql/resources/user/user.resolvers.ts
@@ -4,6 +4,25 @@ import { UserInstance } from "../../../models/UserModel";
 import { Transaction } from "sequelize";
 
 export const userResolvers = {
+  User: {
+    posts: {
+      users: (
+        user,
+        { first = 10, offset = 0 },
+        { db }: { db: DbConnection },
+        info: GraphQLResolveInfo
+      ) => {
+        return db.Post.findAll({
+          where: {
+            author: user.get("id"),
+          },
+          limit: first,
+          offset: offset,
+        });
+      },
+    },
+  },
+
   Query: {
     users: (
       parent,
@@ -16,7 +35,6 @@ export const userResolvers = {
         offset: offset,
       });
     },
-
     user: (
       parent,
       { id },
@@ -25,7 +43,6 @@ export const userResolvers = {
     ) => {
       return db.User.findById(id).then((user: UserInstance) => {
         if (!user) throw new Error(`User with id ${id} not found!`);
-
         return user;
       });
     },
@@ -42,7 +59,6 @@ export const userResolvers = {
         return db.User.create(input, { transaction: t });
       });
     },
-
     updateUser: (
       parent,
       { id, input },
@@ -53,12 +69,10 @@ export const userResolvers = {
       return db.sequelize.transaction((t: Transaction) => {
         return db.User.findById(id).then((user: UserInstance) => {
           if (!user) throw new Error(`User with id ${id} not found!`);
-
           return user.update(input, { transaction: t });
         });
       });
     },
-
     updateUserPassword: (
       parent,
       { id, input },
@@ -69,12 +83,10 @@ export const userResolvers = {
       return db.sequelize.transaction((t: Transaction) => {
         return db.User.findById(id).then((user: UserInstance) => {
           if (!user) throw new Error(`User with id ${id} not found!`);
-
           return user.update(input, { transaction: t }).then((u: any) => !!u);
         });
       });
     },
-
     deleteUser: (
       parent,
       { id, input },
@@ -82,11 +94,9 @@ export const userResolvers = {
       info: GraphQLResolveInfo
     ) => {
       id = parseInt(id);
-
       return db.sequelize.transaction((t: Transaction) => {
         return db.User.findById(id).then((user: UserInstance) => {
           if (!user) throw new Error(`User with id ${id} not found!`);
-
           return user.destroy({ transaction: t }).then((u: any) => !!u);
         });
       });
